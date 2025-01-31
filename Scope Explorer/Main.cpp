50eeea7f2f49e5d418f5600fe1706802c83d0b9a
JoshuaWorthy2024 <joshuaworthy@cgspectrum.institute>
2025-01-31 06:15:00 -0600

    Attempt 1

--- Scope Explorer/Main.cpp
+++ Scope Explorer/Main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
+// Objective: Understand how variable scope affects program behavior.
 
+int x = 0;
+
+void testMethod() {
+	int x = 10;
+
+	std::cout << "Test Method X = " << x << std::endl;
+}
+int main() {
+	int x = 5;
+
+	std::cout << "Main Function X = " << x << std::endl;
+	std::cout << "Global X = " << ::x << std::endl;
+
+	testMethod();
 }
